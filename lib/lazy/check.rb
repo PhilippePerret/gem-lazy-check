9af4bd3f251b420ea7a683f7d530e3fe6934107c
PhilippePerret <philippe.perret@yahoo.fr>
2023-09-25 16:58:58 +0200

    Premier dépôt

---
+++ lib/lazy/check.rb
@@ -0,0 +1,8 @@
+require "lazy/check/version"
+
+module Lazy
+  module Check
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/lazy/check/version.rb
@@ -0,0 +1,5 @@
+module Lazy
+  module Check
+    VERSION = "0.1.0"
+  end
+end
